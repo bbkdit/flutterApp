2d935631a95f5b20fad3e2079ce658d60cbed8b1
Bibek Das <dasbibek189@gmail.com>
2021-09-27 01:05:53 +0530

    first commit

---
+++ lib/color.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColor{
+  // static Color homePageBackground = const Color(0xffaabbcc);
+  static Color homePageBackground = const Color.fromARGB(255, 255, 255, 255);
+}
